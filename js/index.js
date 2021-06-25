a6631c109e079a6f1da47e354ac0ec876ee4267f
Aryan Gera <aryangera@hotmail.com>
2021-06-25 16:11:30 -0400

    save

--- js/index.js
+++ js/index.js
@@ -5,8 +5,78 @@
 //   console.log(res);
 // });
 
-
-
 //on play click hide play button
 //display category buttons
 
+$("#cat-btn").on("click", function () {
+  //clear the main-game screen
+  //MAYBE later add stinger transtion
+
+  const cats = [];
+  const foods = [];
+  const food_ones = [];
+  const food_twos = [];
+  const food_threes = [];
+  const food_fours = [];
+  const food_fives = [];
+  const food_tens = [];
+  const gameFrame = $("#main-game");
+
+  const thisVal = $(this).val()
+
+  $.ajax({
+    //FOOD
+    url: "http://jservice.io/api/category?id=49",
+    method: "GET",
+  }).then(function (res) {
+    const clueArr = res.clues;
+    clueArr.sort((a, b) => a.value - b.value);
+
+    for (let i = 0; i < clueArr.length; i++) {
+      if (clueArr[i].value == null) {
+        clueArr.splice(i, 1);
+        res.clues_count--;
+      } else if (clueArr[i].value == 100) {
+        food_ones.push(clueArr.splice(i, 1));
+        res.clues_count--;
+      } else if (clueArr[i].value == 200) {
+        food_twos.push(clueArr.splice(i, 1));
+        res.clues_count--;
+      } else if (clueArr[i].value == 300) {
+        food_threes.push(clueArr.splice(i, 1));
+        res.clues_count--;
+      } else if (clueArr[i].value == 400) {
+        food_fours.push(clueArr.splice(i, 1));
+        res.clues_count--;
+      } else if (clueArr[i].value == 500) {
+        food_fives.push(clueArr.splice(i, 1));
+        res.clues_count--;
+      } else if (clueArr[i].value == 1000) {
+        food_tens.push(clueArr.splice(i, 1));
+        res.clues_count--;
+      }
+    }
+    foods.push(
+      "Foods",
+      food_ones,
+      food_twos,
+      food_threes,
+      food_fours,
+      food_fives,
+      food_tens
+    );
+    cats.push(foods, ["name", 0]);
+    console.log(cats);
+
+    if (thisVal == "play") {
+      gameFrame.empty();
+      console.log("here", cats.length);
+      for (let i = 0; i < cats.length; i++) {
+        newBtn = $("<button id='cat-btn'>" + cats[i][0] + "</button>")
+        gameFrame.append(newBtn)
+
+      }
+      
+    }
+  });
+});
