7b2bcda580627f1581dee882890cd93309025d40
Aryan Gera <aryangera@hotmail.com>
2021-06-25 15:36:25 -0400

    addnig previous files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// $.ajax({
+//   url: "http://jservice.io/api/category?id=49",
+//   method: "GET",
+// }).then(function (res) {
+//   console.log(res);
+// });
+
+
+
+//on play click hide play button
+//display category buttons
+
